2d5e8094f4ce92d47e3197dc47e2927d0f0be2b1
david <me@davidvlijmincx.com>
2026-05-21 11:07:28 +0200

    trigger build for windows

--- libs/mainframe.c
+++ libs/mainframe.c
@@ -11,7 +11,6 @@ void print_with_prefix(const char *str) {
     printf("%s%s\n", "MU/TH/TR: ", str);
 }
 
-
 // Assignment 1:
 int connect_to_mainframe(int access_code) {
     if (access_code == 42) {
